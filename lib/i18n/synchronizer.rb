6ae1335d6691fc22e84038c614ad5a011845e48e
Damien Danglard <danglard@cgddskl02109.cegidgroup.local>
2020-08-10 11:38:02 +0200

    Initial commit

---
+++ lib/i18n/synchronizer.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require "i18n/synchronizer/version"
+
+module I18n
+  module Synchronizer
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ spec/i18n/synchronizer_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe I18n::Synchronizer do
+  it "has a version number" do
+    expect(I18n::Synchronizer::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
